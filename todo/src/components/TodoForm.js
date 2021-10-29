e60f56db794748787790c1fba3e4bd889c3fd2ef
hilmisarioglu <77903324+hilmisarioglu@users.noreply.github.com>
2021-10-29 22:04:25 +0200

    v5

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,15 +1,20 @@
 import React,{useState} from 'react'
 
-const TodoForm = () => {
+const TodoForm = (props) => {
     const[input, setInput ] = useState('');
 
     const handleChange = e =>{
         setInput(e.target.value)
+        console.log(input)
  
     }
     const handleSubmit = e => {
         e.preventDefault();
-        setInput('')
+        props.onSubmit({
+            text : input,
+            id: Math.floor(Math.random()*10000), 
+        });
+        setInput('');
         console.log({input})
     }
 
--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -3,10 +3,16 @@ import TodoForm from './TodoForm'
 
 const TodoList = () => {
     const[todos,setTodos] = useState([]);
+    
+    const addTodo = todo => {
+        const newTodos = [todo, ...todos]
+        setTodos(newTodos);
+        console.log(...todos)
+    }
     return (
         <div>
             <h1>Whats the plan for today</h1>
-            <TodoForm/>
+            <TodoForm onSubmit={addTodo} />
         </div>
     )
 }
