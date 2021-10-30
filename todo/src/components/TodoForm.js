3311b69d910e9a18d50e6c8bdcd46b2f7cb3f7c6
hilmisarioglu <77903324+hilmisarioglu@users.noreply.github.com>
2021-10-30 14:57:41 +0200

    v7 css added

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,8 +1,14 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect, useRef} from 'react'
 
 const TodoForm = (props) => {
 
     const[input, setInput ] = useState('');
+
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        inputRef.current.focus()
+    })
 
     const handleChange = e =>{
         setInput(e.target.value)
@@ -27,7 +33,8 @@ const TodoForm = (props) => {
             name = 'text'
             className = 'todo-input'
             onChange={handleChange}
-                   // The above code displays a single input field which, when typed in, passes its current value to the handleChange function
+                // The above code displays a single input field which, when typed in, passes its current value to the handleChange function
+            ref={inputRef}
             />
         <button className='todo-button'>Add Todo</button>
         </form>
