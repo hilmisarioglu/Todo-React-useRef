79e08fb9c8297a96505c7dbadccf27e35b972d67
hilmisarioglu <77903324+hilmisarioglu@users.noreply.github.com>
2021-10-30 15:12:28 +0200

    v8 end

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,44 +1,61 @@
-import React,{useState, useEffect, useRef} from 'react'
+import React, { useState, useEffect, useRef } from "react";
 
 const TodoForm = (props) => {
+  const [input, setInput] = useState('');
 
-    const[input, setInput ] = useState('');
+  const inputRef = useRef(null);
 
-    const inputRef = useRef(null);
+  useEffect(() => {
+    inputRef.current.focus();
+  });
 
-    useEffect(() => {
-        inputRef.current.focus()
-    })
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
 
-    const handleChange = e =>{
-        setInput(e.target.value)
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.onSubmit({
+      text: input,
+      id: Math.floor(Math.random() * 10000),
+    });
+    console.log({ input });
+    setInput("");
+  };
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        props.onSubmit({
-            text : input,
-            id: Math.floor(Math.random()*10000), 
-        });
-        console.log({input})
-        setInput('');
-    }
-
-    return (
-        <form className = 'todo-form' onSubmit = {handleSubmit}>
-            <input 
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      {props.edit ? (
+        <>
+          <input
+            type="text"
+            placeholder="Update your item"
+            value={input}
+            name="text"
+            className="todo-input edit"
+            onChange={handleChange}
+            // The above code displays a single input field which, when typed in, passes its current value to the handleChange function
+            ref={inputRef}
+          />
+          <button className="todo-button edit">Update</button>
+        </>
+      ) : (
+        <>
+          <input
             type="text"
             placeholder="Add a todo"
-            value = {input}
-            name = 'text'
-            className = 'todo-input'
+            value={input}
+            name="text"
+            className="todo-input"
             onChange={handleChange}
-                // The above code displays a single input field which, when typed in, passes its current value to the handleChange function
+            // The above code displays a single input field which, when typed in, passes its current value to the handleChange function
             ref={inputRef}
-            />
-        <button className='todo-button'>Add Todo</button>
-        </form>
-    )
-}
+          />
+          <button className="todo-button">Add Todo</button>
+        </>
+      )}
+    </form>
+  );
+};
 
-export default TodoForm
+export default TodoForm;
